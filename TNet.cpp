c3101ce9231629fa8e3d02bbf08b7ffd471f6e67
davidepatti <davidepatti@c6fdfaa7-a24f-5be2-3a33-356e4eb07430>
2012-03-27 13:58:28 +0000

    - Drafting implementation of starting segment request

--- TNet.cpp
+++ TNet.cpp
@@ -90,11 +90,17 @@ void TNet::buildMesh()
     {
       t[i][0]->r->reservation_table.invalidate(DIRECTION_NORTH);
       t[i][TGlobalParams::mesh_dim_y-1]->r->reservation_table.invalidate(DIRECTION_SOUTH);
+
+      t[i][0]->r->disr.invalidate(DIRECTION_NORTH);
+      t[i][TGlobalParams::mesh_dim_y-1]->r->disr.invalidate(DIRECTION_SOUTH);
     }
   for(int j=0; j<TGlobalParams::mesh_dim_y; j++)
     {
       t[0][j]->r->reservation_table.invalidate(DIRECTION_WEST);
       t[TGlobalParams::mesh_dim_x-1][j]->r->reservation_table.invalidate(DIRECTION_EAST);
+
+      t[0][j]->r->disr.invalidate(DIRECTION_WEST);
+      t[TGlobalParams::mesh_dim_x-1][j]->r->disr.invalidate(DIRECTION_EAST);
     }
 
 }
