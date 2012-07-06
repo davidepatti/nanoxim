9d0ec7574947e1fe907e760e060015790b40327f
xedivad@gmail.com <xedivad@gmail.com@c6fdfaa7-a24f-5be2-3a33-356e4eb07430>
2012-07-06 12:31:21 +0000

    - Added Stats/GlobalStats

---
+++ GlobalStats.cpp
@@ -0,0 +1,280 @@
+/*
+ * Noxim - the NoC Simulator
+ *
+ * (C) 2005-2010 by the University of Catania
+ * For the complete list of authors refer to file ../doc/AUTHORS.txt
+ * For the license applied to these sources refer to file ../doc/LICENSE.txt
+ *
+ * This file contains the implementaton of the global statistics
+ */
+
+#include "GlobalStats.h"
+using namespace std;
+
+GlobalStats::GlobalStats(const TNet * _net)
+{
+    net = _net;
+
+#ifdef TESTING
+    drained_total = 0;
+#endif
+}
+
+double GlobalStats::getAverageDelay()
+{
+    unsigned int total_packets = 0;
+    double avg_delay = 0.0;
+
+    for (int y = 0; y < GlobalParams::mesh_dim_y; y++)
+	for (int x = 0; x < GlobalParams::mesh_dim_x; x++) {
+	    unsigned int received_packets =
+		net->t[x][y]->r->stats.getReceivedPackets();
+
+	    if (received_packets) {
+		avg_delay +=
+		    received_packets *
+		    net->t[x][y]->r->stats.getAverageDelay();
+		total_packets += received_packets;
+	    }
+	}
+
+    avg_delay /= (double) total_packets;
+
+    return avg_delay;
+}
+
+double GlobalStats::getAverageDelay(const int src_id,
+					 const int dst_id)
+{
+    TNode *node = net->searchNode(dst_id);
+
+    assert(node != NULL);
+
+    return node->r->stats.getAverageDelay(src_id);
+}
+
+double GlobalStats::getMaxDelay()
+{
+    double maxd = -1.0;
+
+    for (int y = 0; y < GlobalParams::mesh_dim_y; y++)
+	for (int x = 0; x < GlobalParams::mesh_dim_x; x++) {
+	    TCoord coord;
+	    coord.x = x;
+	    coord.y = y;
+	    int node_id = coord2Id(coord);
+	    double d = getMaxDelay(node_id);
+	    if (d > maxd)
+		maxd = d;
+	}
+
+    return maxd;
+}
+
+double GlobalStats::getMaxDelay(const int node_id)
+{
+    TCoord coord = id2Coord(node_id);
+
+    unsigned int received_packets =
+	net->t[coord.x][coord.y]->r->stats.getReceivedPackets();
+
+    if (received_packets)
+	return net->t[coord.x][coord.y]->r->stats.getMaxDelay();
+    else
+	return -1.0;
+}
+
+double GlobalStats::getMaxDelay(const int src_id, const int dst_id)
+{
+    TNode *node = net->searchNode(dst_id);
+
+    assert(node != NULL);
+
+    return node->r->stats.getMaxDelay(src_id);
+}
+
+vector < vector < double > > GlobalStats::getMaxDelayMtx()
+{
+    vector < vector < double > > mtx;
+
+    mtx.resize(GlobalParams::mesh_dim_y);
+    for (int y = 0; y < GlobalParams::mesh_dim_y; y++)
+	mtx[y].resize(GlobalParams::mesh_dim_x);
+
+    for (int y = 0; y < GlobalParams::mesh_dim_y; y++)
+	for (int x = 0; x < GlobalParams::mesh_dim_x; x++) {
+	    TCoord coord;
+	    coord.x = x;
+	    coord.y = y;
+	    int id = coord2Id(coord);
+	    mtx[y][x] = getMaxDelay(id);
+	}
+
+    return mtx;
+}
+
+double GlobalStats::getAverageThroughput(const int src_id,
+					      const int dst_id)
+{
+    TNode *node = net->searchNode(dst_id);
+
+    assert(node != NULL);
+
+    return node->r->stats.getAverageThroughput(src_id);
+}
+
+double GlobalStats::getAverageThroughput()
+{
+    unsigned int total_comms = 0;
+    double avg_throughput = 0.0;
+
+    for (int y = 0; y < GlobalParams::mesh_dim_y; y++)
+	for (int x = 0; x < GlobalParams::mesh_dim_x; x++) {
+	    unsigned int ncomms =
+		net->t[x][y]->r->stats.getTotalCommunications();
+
+	    if (ncomms) {
+		avg_throughput +=
+		    ncomms * net->t[x][y]->r->stats.getAverageThroughput();
+		total_comms += ncomms;
+	    }
+	}
+
+    avg_throughput /= (double) total_comms;
+
+    return avg_throughput;
+}
+
+unsigned int GlobalStats::getReceivedPackets()
+{
+    unsigned int n = 0;
+
+    for (int y = 0; y < GlobalParams::mesh_dim_y; y++)
+	for (int x = 0; x < GlobalParams::mesh_dim_x; x++)
+	    n += net->t[x][y]->r->stats.getReceivedPackets();
+
+    return n;
+}
+
+unsigned int GlobalStats::getReceivedFlits()
+{
+    unsigned int n = 0;
+
+    for (int y = 0; y < GlobalParams::mesh_dim_y; y++)
+	for (int x = 0; x < GlobalParams::mesh_dim_x; x++) {
+	    n += net->t[x][y]->r->stats.getReceivedFlits();
+#ifdef TESTING
+	    drained_total += net->t[x][y]->r->local_drained;
+#endif
+	}
+
+    return n;
+}
+
+double GlobalStats::getThroughput()
+{
+    assert(false);
+    int total_cycles;
+    /*
+    int total_cycles =
+	GlobalParams::simulation_time - GlobalParams::stats_warm_up_time;
+
+	*/
+    //  int number_of_ip = GlobalParams::mesh_dim_x * GlobalParams::mesh_dim_y;
+    //  return (double)getReceivedFlits()/(double)(total_cycles * number_of_ip);
+
+    unsigned int n = 0;
+    unsigned int trf = 0;
+    for (int y = 0; y < GlobalParams::mesh_dim_y; y++)
+	for (int x = 0; x < GlobalParams::mesh_dim_x; x++) {
+	    unsigned int rf = net->t[x][y]->r->stats.getReceivedFlits();
+
+	    if (rf != 0)
+		n++;
+
+	    trf += rf;
+	}
+    return (double) trf / (double) (total_cycles * n);
+
+}
+
+vector < vector < unsigned long > > GlobalStats::getRoutedFlitsMtx()
+{
+
+    vector < vector < unsigned long > > mtx;
+
+    mtx.resize(GlobalParams::mesh_dim_y);
+    /*
+    for (int y = 0; y < GlobalParams::mesh_dim_y; y++)
+	mtx[y].resize(GlobalParams::mesh_dim_x);
+
+    for (int y = 0; y < GlobalParams::mesh_dim_y; y++)
+	for (int x = 0; x < GlobalParams::mesh_dim_x; x++)
+	    mtx[y][x] = net->t[x][y]->r->getRoutedFlits();
+	    */
+
+    return mtx;
+}
+
+double GlobalStats::getPower()
+{
+    double power = 0.0;
+
+    /*
+    for (int y = 0; y < GlobalParams::mesh_dim_y; y++)
+	for (int x = 0; x < GlobalParams::mesh_dim_x; x++)
+	    power += net->t[x][y]->r->getPower();
+	    */
+
+    return power;
+}
+
+void GlobalStats::showStats(std::ostream & out )
+{
+    out << "% Total received packets: " << getReceivedPackets() << endl;
+    out << "% Total received flits: " << getReceivedFlits() << endl;
+    out << "% Global average delay (cycles): " << getAverageDelay() <<
+	endl;
+    out << "% Global average throughput (flits/cycle): " <<
+	getAverageThroughput() << endl;
+    //out << "% Throughput (flits/cycle/IP): " << getThroughput() << endl;
+    out << "% Max delay (cycles): " << getMaxDelay() << endl;
+    out << "% Total energy (J): " << getPower() << endl;
+
+    /*
+    if (detailed) {
+	out << endl << "detailed = [" << endl;
+	for (int y = 0; y < GlobalParams::mesh_dim_y; y++)
+	    for (int x = 0; x < GlobalParams::mesh_dim_x; x++)
+		net->t[x][y]->r->stats.showStats(y *
+						 GlobalParams::
+						 mesh_dim_x + x, out,
+						 true);
+	out << "];" << endl;
+
+	// show MaxDelay matrix
+	vector < vector < double > > md_mtx = getMaxDelayMtx();
+
+	out << endl << "max_delay = [" << endl;
+	for (unsigned int y = 0; y < md_mtx.size(); y++) {
+	    out << "   ";
+	    for (unsigned int x = 0; x < md_mtx[y].size(); x++)
+		out << setw(6) << md_mtx[y][x];
+	    out << endl;
+	}
+	out << "];" << endl;
+
+	// show RoutedFlits matrix
+	vector < vector < unsigned long > > rf_mtx = getRoutedFlitsMtx();
+
+	out << endl << "routed_flits = [" << endl;
+	for (unsigned int y = 0; y < rf_mtx.size(); y++) {
+	    out << "   ";
+	    for (unsigned int x = 0; x < rf_mtx[y].size(); x++)
+		out << setw(10) << rf_mtx[y][x];
+	    out << endl;
+	}
+	out << "];" << endl;
+    }
+    */
+}
---
+++ Stats.cpp
@@ -0,0 +1,220 @@
+
+#include "Stats.h"
+
+// TODO: nan in averageDelay
+
+void Stats::configure(const int node_id, const double _warm_up_time)
+{
+    id = node_id;
+    warm_up_time = _warm_up_time;
+}
+
+void Stats::receivedFlit(const double arrival_time,
+			      const TPacket & packet)
+{
+    if (arrival_time - DEFAULT_RESET_TIME < warm_up_time)
+	return;
+
+    int i = searchCommHistory(packet.src_id);
+
+    if (i == -1) {
+	// first packet received from a given source
+	// initialize CommHist structure
+	CommHistory ch;
+
+	ch.src_id = packet.src_id;
+	ch.total_received_flits = 0;
+	chist.push_back(ch);
+
+	i = chist.size() - 1;
+    }
+
+    if (packet.type == SEGMENT_REQUEST)
+	chist[i].delays.push_back(arrival_time - packet.timestamp);
+
+    chist[i].total_received_flits++;
+    chist[i].last_received_flit_time = arrival_time - warm_up_time;
+}
+
+double Stats::getAverageDelay(const int src_id)
+{
+    double sum = 0.0;
+
+    int i = searchCommHistory(src_id);
+
+    assert(i >= 0);
+
+    for (unsigned int j = 0; j < chist[i].delays.size(); j++)
+	sum += chist[i].delays[j];
+
+    return sum / (double) chist[i].delays.size();
+}
+
+double Stats::getAverageDelay()
+{
+    double avg = 0.0;
+
+    for (unsigned int k = 0; k < chist.size(); k++) {
+	unsigned int samples = chist[k].delays.size();
+	if (samples)
+	    avg += (double) samples *getAverageDelay(chist[k].src_id);
+    }
+
+    return avg / (double) getReceivedPackets();
+}
+
+double Stats::getMaxDelay(const int src_id)
+{
+    double maxd = -1.0;
+
+    int i = searchCommHistory(src_id);
+
+    assert(i >= 0);
+
+    for (unsigned int j = 0; j < chist[i].delays.size(); j++)
+	if (chist[i].delays[j] > maxd) {
+	    //      cout << src_id << " -> " << id << ": " << chist[i].delays[j] << endl;
+	    maxd = chist[i].delays[j];
+	}
+    return maxd;
+}
+
+double Stats::getMaxDelay()
+{
+    double maxd = -1.0;
+
+    for (unsigned int k = 0; k < chist.size(); k++) {
+	unsigned int samples = chist[k].delays.size();
+	if (samples) {
+	    double m = getMaxDelay(chist[k].src_id);
+	    if (m > maxd)
+		maxd = m;
+	}
+    }
+
+    return maxd;
+}
+
+double Stats::getAverageThroughput(const int src_id)
+{
+    int i = searchCommHistory(src_id);
+
+    assert(i >= 0);
+
+    if (chist[i].total_received_flits == 0)
+	return -1.0;
+    else
+	return (double) chist[i].total_received_flits /
+	    (double) chist[i].last_received_flit_time;
+}
+
+double Stats::getAverageThroughput()
+{
+    double sum = 0.0;
+
+    for (unsigned int k = 0; k < chist.size(); k++) {
+	double avg = getAverageThroughput(chist[k].src_id);
+	if (avg > 0.0)
+	    sum += avg;
+    }
+
+    return sum;
+}
+
+unsigned int Stats::getReceivedPackets()
+{
+    int n = 0;
+
+    for (unsigned int i = 0; i < chist.size(); i++)
+	n += chist[i].delays.size();
+
+    return n;
+}
+
+unsigned int Stats::getReceivedFlits()
+{
+    int n = 0;
+
+    for (unsigned int i = 0; i < chist.size(); i++)
+	n += chist[i].total_received_flits;
+
+    return n;
+}
+
+unsigned int Stats::getTotalCommunications()
+{
+    return chist.size();
+}
+
+double Stats::getCommunicationEnergy(int src_id, int dst_id)
+{
+    // Assumptions: minimal path routing, constant packet size
+
+    TCoord src_coord = id2Coord(src_id);
+    TCoord dst_coord = id2Coord(dst_id);
+
+    int hops =
+	abs(src_coord.x - dst_coord.x) + abs(src_coord.y - dst_coord.y);
+
+    /*
+    double energy =
+	hops * ((power.getPwrForward() +
+		 power.getPwrIncoming()) *
+		(GlobalParams::min_packet_size +
+		 GlobalParams::max_packet_size) / 2 +
+		power.getPwrRouting() + power.getPwrSelection()
+	);
+	*/
+
+    assert(false);
+    return 0;
+    //return energy;
+}
+
+int Stats::searchCommHistory(int src_id)
+{
+    for (unsigned int i = 0; i < chist.size(); i++)
+	if (chist[i].src_id == src_id)
+	    return i;
+
+    return -1;
+}
+
+void Stats::showStats(int curr_node, std::ostream & out, bool header)
+{
+    if (header) {
+	out << "%"
+	    << setw(5) << "src"
+	    << setw(5) << "dst"
+	    << setw(10) << "delay avg"
+	    << setw(10) << "delay max"
+	    << setw(15) << "throughput"
+	    << setw(13) << "energy"
+	    << setw(12) << "received" << setw(12) << "received" << endl;
+	out << "%"
+	    << setw(5) << ""
+	    << setw(5) << ""
+	    << setw(10) << "cycles"
+	    << setw(10) << "cycles"
+	    << setw(15) << "packets/cycle"
+	    << setw(13) << "Joule"
+	    << setw(12) << "packets" << setw(12) << "packets" << endl;
+    }
+    for (unsigned int i = 0; i < chist.size(); i++) {
+	out << " "
+	    << setw(5) << chist[i].src_id
+	    << setw(5) << curr_node
+	    << setw(10) << getAverageDelay(chist[i].src_id)
+	    << setw(10) << getMaxDelay(chist[i].src_id)
+	    << setw(15) << getAverageThroughput(chist[i].src_id)
+	    << setw(13) << getCommunicationEnergy(chist[i].src_id,
+						  curr_node)
+	    << setw(12) << chist[i].delays.size()
+	    << setw(12) << chist[i].total_received_flits << endl;
+    }
+
+    out << "% Aggregated average delay (cycles): " << getAverageDelay() <<
+	endl;
+    out << "% Aggregated average throughput (packets/cycle): " <<
+	getAverageThroughput() << endl;
+}
